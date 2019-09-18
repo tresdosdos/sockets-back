1884b72fd6f334c1a858a7c683a52b8ad543a135
Vitali Dziachuk <vitali.dziachuk@itechart-group.com>
2019-09-18 19:15:54 +0300

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const io = require('socket.io')();
+const server = require('http').createServer();
+
+io.attach(server, {
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
+
+io.on('connection', (socket) => {
+    console.log('connected');
+
+    socket.on('text', (data) => {
+        console.log("Received text:", data);
+    });
+});
+
+
+
+server.listen(8080);
